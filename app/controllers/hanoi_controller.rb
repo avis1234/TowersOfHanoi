1c68697b6356b244ab49d7b682ff0cf8bd586a8e
Avi Segal <avisegal@gmail.com>
2011-10-04 11:40:19 +0200

    change

--- app/controllers/hanoi_controller.rb
+++ app/controllers/hanoi_controller.rb
@@ -1,14 +1,26 @@
 class HanoiController < ApplicationController
   def index
+      # default behaviour rendering erb or
+      # call render ; redirect_to ; head
   end
 
   def set_rings
   end
 
   def start
+    # get number of rings
+    # call model to calculate all moves and populate list
+    # view will draw rings on column A
+    # gray Rings entry field
+    # gray Start button
+    # enable NEXT MOVE  button
+
   end
 
   def next_move
+    # Take needed operation for list
+    # Activate View to draw move
+    # if last move, enable Ring entry field and Start Button and Disable Next Move
   end
 
 end
--- app/models/hanoi.rb
+++ app/models/hanoi.rb
@@ -1,2 +1,19 @@
 class Hanoi < ActiveRecord::Base
+
+  def initialize(r)
+    @rings = r
+    @moves = Array.new
+
+  end
+
+  def get_going (from, to, rings)
+    if rings == 1
+        @moves << [from , to]
+      else
+        mid = 6-from-to
+        get_going(from, mid, rings-1)
+        @moves << [from , to]
+        get_going(mid, to, rings-1)
+      end
+    end
 end
