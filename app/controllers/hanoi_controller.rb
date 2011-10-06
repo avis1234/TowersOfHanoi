f62ad4c300e26677ba62d72c856545122a69562d
Avi Segal <avisegal@gmail.com>
2011-10-06 15:11:01 +0200

    Added Next Step for Rings

--- app/controllers/hanoi_controller.rb
+++ app/controllers/hanoi_controller.rb
@@ -12,21 +12,26 @@ class HanoiController < ApplicationController
   def start
     # get Hanoi instance from db
    @hn1 = Hanoi.new
-   @hn1.rngs = params[:rngs]
+   @hn1.rngs = Integer(params[:hanoi][:rngs])
     # number of rings in model already
     # call model to calculate all moves and populate list
+   @hn1.set_towers(@hn1.rngs)
    @hn1.get_going(1,3, @hn1.rngs)
    @hn1.save
     # view will draw rings on column A
     # gray Rings entry field, gray Start button, enable NEXT MOVE button
     # achieved by rendered view page
-
   end
 
   def next_move
-    # Take needed operation for list
+    @hn1 = Hanoi.find()
+    # TMake next move
+    finished = @hn1.next_move
     # Activate View to draw move
     # if last move, enable Ring entry field and Start Button and Disable Next Move
+    if finished
+      puts "Finished!!"
+      render "finished"
+    end
   end
-
 end
--- app/models/hanoi.rb
+++ app/models/hanoi.rb
@@ -1,20 +1,42 @@
 class Hanoi < ActiveRecord::Base
   attr_accessor :rngs
+  attr_accessor :towers
+
   serialize :moves
+  serialize :towers
 
   def initialize
     @moves = Array.new
+    @towers = Array.new
     @rngs = 3
+    @steps = 0
+  end
+
+  def set_towers (rings)
+    @tower[1] = rings
+    @tower[2] = 0
+    @tower[3] = 0
+    puts "Number of Rings is #{rings}"
+  end
+
+  def next_move
+    from = @moves[@steps][:from]
+    from = @moves[@steps][:to]
+    puts "Move one ring from #{from} to #{to}"
+    @tower[from] = @tower[from] - 1
+    @tower[to] =  @tower[to] + 1
+    @steps = @steps + 1
+    return (towers[1] + towers[2] = 0)
   end
 
   def get_going (from, to, rings)
     if rings == 1
-        @moves << [from , to]
-      else
+        @moves << [:from, from , :to, to]
+    else
         mid = 6-from-to
         get_going(from, mid, rings-1)
-        @moves << [from , to]
+        @moves << [:from, from , :to, to]
         get_going(mid, to, rings-1)
-      end
     end
+  end
 end
